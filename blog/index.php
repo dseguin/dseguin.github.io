fb00d72d625b886b3a541a7e0351b729182a99af
David Seguin <davidseguin@live.ca>
2020-02-21 15:27:36 -0500

    Moved blog index to PHP
    - Changed About page information
    - Generate blog entries using PHP
    - Added first post "Initial_Commit"
    - Changed page titles

--- blog/index.php
+++ blog/index.php
@@ -1,11 +1,23 @@
-<!DOCTYPE html>
+<?php
+function dump_posts() {
+	$files = glob("*/*/*.html");
+	array_multisort(array_map('filectime', $files), SORT_NUMERIC, SORT_DESC, $files);
+	for($i=0;$i<count($files);$i++) {
+		$f = file_get_contents($files[$i]);
+		if(!$f) continue;
+		if(!preg_match("/<h1 class=\"post_title\">(.*)<\/h1>/siU", $f, $match)) continue;
+		echo "<tr><td>" . date('Y-m-d', filectime($files[$i])) . "</td><td><a href=\"" . $files[$i] . "\">" . trim($match[1]) . "</a></td></tr>";
+	}
+}
+?>
 <html lang="en">
 <head>
 	<meta charset="utf-8"/>
-	<title>printf(&quot;Thoughts&quot;);</title>
+	<title>printf(&quot;Thoughts&quot;); - Blog</title>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Share Tech Mono"/>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT Sans"/>
 	<link rel="stylesheet" href="/css/main.css"/>
+	<link rel="stylesheet" href="/css/blog_index.css"/>
 </head>
 <body>
 <div id="all_wrapper">
@@ -26,7 +38,12 @@
 	<!--CONTENT-->
 	<div id="content_wrapper">
 		<h1>Thoughts</h1>
-		<p>Oops, this page is not ready yet!</p>
+		<table>
+		<tr><th>Date</th><th>Title</th></tr>
+		<?php
+			dump_posts();
+		?>
+		</table>
 	</div><!--content_wrapper-->
 </div><!--all_wrapper-->
 <div id="footer_wrapper">
